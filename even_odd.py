29db83302fafd3eb1ca4b646bcf2aef2f615064e
Shreya Raykar <shreyaraikar2304@gmail.com>
2025-11-27 15:28:00 +0530

    Update even_odd.py

--- even_odd.py
+++ even_odd.py
@@ -1,13 +1,16 @@
 import sys
-args = sys.argv[1:]
-numbers = [int(num) for num in args]
-even = 0
-odd = 0
-for n in numbers:
-    if n % 2 == 0:
-        even += 1
-    else:
-        odd += 1
-print("Numbers:", numbers)
+if len(sys.argv)==2:
+    script_name=sys.argv[0]
+    num=int(sys.argv[1])
+else:
+    script_name=sys.argv[0]
+    num=23
+even=0
+odd=0
+if num % 2 == 0:
+    even=num
+else:
+    odd =num
+print("Numbers:", num)
 print("Count of Even Numbers:", even)
 print("Count of Odd Numbers:", odd)
